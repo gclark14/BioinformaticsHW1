6df75aba76edd462a0301cc76fad80ace0d3cf34
gclark14 <unrivalled.dh@gmail.com>
2019-09-13 11:00:23 -0700

    merge master

--- cmake-build-debug/CMakeFiles/BioinformaticsHW1.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/BioinformaticsHW1.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "C:/Users/Gabriel/CLionProjects/BioinformaticsHW_1/main.cpp" "C:/Users/Gabriel/CLionProjects/BioinformaticsHW_1/cmake-build-debug/CMakeFiles/BioinformaticsHW1.dir/main.cpp.obj"
+  "C:/Users/unriv/CLionProjects/BioinformaticsHW1/main.cpp" "C:/Users/unriv/CLionProjects/BioinformaticsHW1/cmake-build-debug/CMakeFiles/BioinformaticsHW1.dir/main.cpp.obj"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
